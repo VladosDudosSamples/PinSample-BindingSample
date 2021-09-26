0fd148f93c8b8ef97030f98d1acd50aa5ded6c44
Владислав Чийпеш <v.toper17@gmail.com>
2021-09-26 15:26:42 +0300

    biometric was added

--- app/src/main/java/com/example/pincode/common/activities/MainActivity.kt
+++ app/src/main/java/com/example/pincode/common/activities/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.pincode.common.activities
 
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.pincode.R
+import android.os.CancellationSignal
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.pincode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var cancellationSignal: CancellationSignal? = null
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback
+        get() =
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                    super.onAuthenticationError(errorCode, errString)
+                    notifyUser("Authentication error: $errString")
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                    super.onAuthenticationSucceeded(result)
+
+                    notifyUser("Authentication success!")
+                    successfulEnter()
+                }
+            }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -16,10 +40,55 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding
+        checkBiometricSupport()
 
+        binding.fingerPrintBtn.setOnClickListener {
+            val biometricPrompt = BiometricPrompt.Builder(this)
+                .setTitle("Use your biometric")
+                .setSubtitle("Authentication is required")
+                .setDescription("This app use biometric protection to keep your data secure")
+                .setNegativeButton("Cancel", this.mainExecutor, DialogInterface.OnClickListener { di, i ->
+                    notifyUser("Authentication cancelled")
+                }).build()
+            biometricPrompt.authenticate(getCancellationSignal(), mainExecutor, authenticationCallback)
+        }
     }
-    fun successfulEnter(){
+
+    private fun successfulEnter() {
         startActivity(Intent(this, SuccessActivity::class.java))
+    }
+
+    private fun notifyUser(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getCancellationSignal(): CancellationSignal {
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            notifyUser("Authentication was cancelled by user")
+        }
+        return cancellationSignal as CancellationSignal
+    }
+
+    private fun checkBiometricSupport(): Boolean {
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        if (!keyguardManager.isKeyguardSecure) {
+            notifyUser("Fingerprint authentication has`t been enabled in settings")
+            return false
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.USE_BIOMETRIC
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            notifyUser("Fingerprint permission is not enabled")
+            return false
+        }
+
+        return if (packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
+            true
+        } else true
     }
 }
